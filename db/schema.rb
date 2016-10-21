00f687823224f6d79ea2096725c91c4686fcb0ce
argerim <argerim@gmail.com>
2016-10-21 04:50:24 -0200

    drop table models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021061755) do
+ActiveRecord::Schema.define(version: 20161021064931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,13 +39,6 @@ ActiveRecord::Schema.define(version: 20161021061755) do
     t.integer  "webmotors_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-  end
-
-  create_table "models", force: :cascade do |t|
-    t.integer  "make_id"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "car_models", "brands"
