8b1d0447af765e702074d981840e0ef198be119b
argerim <argerim@gmail.com>
2016-10-21 05:14:07 -0200

    refactory controllers and yours views.

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -1,19 +1,26 @@
 class CarModelsController < ApplicationController
+  before_action :brand, only: :index
+
   def index
-    #search the models
     uri = URI("http://www.webmotors.com.br/carro/modelos")
 
-    # Make request for Webmotors site
-    make = Brand.where(webmotors_code: params[:webmotors_make_id])[0]
-
-    response = Net::HTTP.post_form(uri, { marca: params[:webmotors_make_id] })
+    response = Net::HTTP.post_form(uri, { marca: permited_params[:code] })
     models_json = JSON.parse response.body
 
-    # debugger
-
-    # Itera no resultado e grava os modelos que ainda não estão persistidas
     models_json.each do |json|
-      CarModel.create(brand_id: make.id, name: json["Nome"])
+      @brand.car_models.create(name: json["Nome"])
     end
+
+    @car_models = @brand.car_models
   end
+
+  private
+
+    def brand
+      @brand = Brand.find_by(webmotors_code: permited_params[:code])
+    end
+
+    def permited_params
+      params.require(:webmotors).permit(:code)
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,15 @@
 class HomeController < ApplicationController
+
   def index
-    #search the make
     uri = URI("http://www.webmotors.com.br/carro/marcas")
 
-    # Make request for Webmotors site
     response = Net::HTTP.post_form(uri, {})
     json = JSON.parse response.body
 
-    # Itera no resultado e grava as marcas que ainda não estão persistidas
     json.each do |make_params|
       Brand.create(name: make_params["Nome"], webmotors_code: make_params["Id"])
     end
+
+    @brandes = Brand.all
   end
 end
