0310fb3a7621cb3dd2c40540b35bd390a07fbc53
argerim <argerim@gmail.com>
2016-10-21 04:07:30 -0200

    create test model branch.

---
+++ spec/factories/brands.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :brand do
+    name "CHEVROLET"
+    webmotors_code 1
+  end
+end
---
+++ spec/models/brand_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Brand, type: :model do
+
+  subject { create(:brand) }
+
+
+  context 'Validations' do
+    [:name].each do |attr|
+      it { should validate_uniqueness_of(attr).case_insensitive  }
+    end
+    [:webmotors_code].each do |attr|
+      it { should validate_uniqueness_of(attr) }
+    end
+    [:name, :webmotors_code].each do |attr|
+      it { should validate_presence_of(attr) }
+    end
+  end
+end
