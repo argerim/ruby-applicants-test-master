ffa1caeab8a4182978ad1d95089dd3ca0c7cf92a
argerim <argerim@gmail.com>
2016-10-21 15:25:30 -0200

    change make_params to brand_params.

--- app/business/update_brands.rb
+++ app/business/update_brands.rb
@@ -1,8 +1,8 @@
 class UpdateBrands
 
   def initialize
-    WebMotorsService.new(:brand, {}).json_parse.each do |make_params|
-      Brand.create(name: make_params["Nome"], webmotors_code: make_params["Id"])
+    WebMotorsService.new(:brand, {}).json_parse.each do |brand_params|
+      Brand.create(name: brand_params["Nome"], webmotors_code: brand_params["Id"])
     end
   end
 end
